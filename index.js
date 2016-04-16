032b56b18833bbb8814705552ac45da2b30121c4
Jason Miller <jason@developit.ca>
2016-04-16 13:07:37 -0400

    2.0.0: update to modify-babel-preset 2.0, add possibly ridiculous sanity test

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var modify = require('modify-babel-preset');
 
-module.exports = modify('es2015-minimal', {
+var LOOSE = { loose: true };
+
+module.exports = modify('es2015', {
+	// configure relevant plugins into loose mode:
+	'transform-es2015-template-literals': LOOSE,
+	'transform-es2015-classes': LOOSE,
+	'transform-es2015-computed-properties': LOOSE,
+	'transform-es2015-for-of': LOOSE,
+	'transform-es2015-spread': LOOSE,
+	'transform-es2015-destructuring': LOOSE,
+
+	// remove strict plugins:
+	'transform-es2015-function-name': false,
+	'transform-es2015-typeof-symbol': false,
+
 	// remove commonjs transform
 	'transform-es2015-modules-commonjs': false,
 
